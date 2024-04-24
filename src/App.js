bce53165b9bbfcf90de9bbd7b464354de7c64d55
Vincent <vincent.chauvaux@gmail.com>
2024-04-24 11:05:51 +0200

    premier commit de la version du cours 1/2

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} /> 
+            <Route path="*" element={<Home/>} />
+        </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A porpos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, assumenda. Adipisci magnam doloremque porro omnis minima. Illo culpa nostrum sit rem et molestias ducimus porro minima ipsa aperiam, possimus obcaecati fugiat, similique libero quo sunt fuga! Velit ex necessitatibus adipisci praesentium sunt dignissimos porro! Doloribus repudiandae ullam nihil dignissimos voluptatum. Itaque esse non provident debitis nesciunt beatae laborum dolores sint optio voluptatibus eos iure, delectus officia a magnam ipsa nobis, deserunt tempora repudiandae? Eveniet illum animi minima voluptate saepe sint numquam a earum iure quasi? Adipisci, rerum nisi doloremque possimus error, quasi modi, illo accusamus reiciendis asperiores molestiae dolor doloribus.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, assumenda. Adipisci magnam doloremque porro omnis minima. Illo culpa nostrum sit rem et molestias ducimus porro minima ipsa aperiam, possimus obcaecati fugiat, similique libero quo sunt fuga! Velit ex necessitatibus adipisci praesentium sunt dignissimos porro! Doloribus repudiandae ullam nihil dignissimos voluptatum. Itaque esse non provident debitis nesciunt beatae laborum dolores sint optio voluptatibus eos iure, delectus officia a magnam ipsa nobis, deserunt tempora repudiandae? Eveniet illum animi minima voluptate saepe sint numquam a earum iure quasi? Adipisci, rerum nisi doloremque possimus error, quasi modi, illo accusamus reiciendis asperiores molestiae dolor doloribus.</p>
+        </div>
+    );
+};
+
+export default About;
